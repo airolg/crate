0e6353051f3caf853674a3ff01d854ce19920931
Emily Woods <emily@crate.io>
2018-01-28 18:00:02 +0100

    Finish testing CSVLineParser

--- sql/src/main/java/io/crate/operation/collect/files/CSVLineParser.java
+++ sql/src/main/java/io/crate/operation/collect/files/CSVLineParser.java
@@ -16,16 +16,21 @@ import java.util.stream.IntStream;
 
 public class CSVLineParser {
 
+    public static final String ILLEGAL_ARGUMENT_EX_STRING = "Number of row entries is not equal to the number of columns";
+
     public String parse(byte[] header, byte[] row) throws IOException {
 
         CSVParser headerParser = CSVFormat
             .DEFAULT
             .withFirstRecordAsHeader()
+            .withTrim()
             .parse(new InputStreamReader(new ByteArrayInputStream(header), StandardCharsets.UTF_8));
 
         CSVParser rowParser = CSVFormat
             .DEFAULT
+            .withTrim()
             .parse(new InputStreamReader(new ByteArrayInputStream(row), StandardCharsets.UTF_8));
+
 
         final Set<String> keys = headerParser.getHeaderMap().keySet();
 
@@ -40,13 +45,18 @@ public class CSVLineParser {
         JSONObject convertedCsvToJson = new JSONObject();
 
         for (CSVRecord rowEntries : rowParser) {
-            if (rowEntries.size() > keys.size())
-                throw new IllegalArgumentException("Number of row entries exceeds number of columns");
-            List<String> keyList = new ArrayList<>(keys);
+            List<String> keyList = getListOfKeys(keys);
+
+            if (rowEntries.size() != keyList.size()) throw new IllegalArgumentException(ILLEGAL_ARGUMENT_EX_STRING);
             convertedCsvToJson = convertCSVToJsonObject(keyList, rowEntries);
         }
 
         return convertedCsvToJson.toString();
+    }
+
+    private List<String> getListOfKeys(Set<String> keys) {
+        keys.removeIf(item -> item == null || "".equals(item));
+        return new ArrayList<>(keys);
     }
 
     private JSONObject convertCSVToJsonObject(List<String> keys, CSVRecord rowEntries) {
@@ -57,6 +67,10 @@ public class CSVLineParser {
     private Map<String, String> getMapOfKeysAndRowEntries(List<String> keys, CSVRecord rowEntries) {
         return IntStream.range(0, keys.size())
             .boxed()
-            .collect(Collectors.toMap(keys::get, rowEntries::get));
+            .collect(Collectors.toMap(
+                keys::get,
+                rowEntries::get,
+                (key, value) -> { throw new IllegalArgumentException(ILLEGAL_ARGUMENT_EX_STRING); },
+                LinkedHashMap::new));
     }
 }
